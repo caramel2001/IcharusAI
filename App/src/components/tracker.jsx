3ae1ed6af24907200682b76e834ded80d5d858e8
Pratham <agarwalpratham2001@gmail.com>
2024-03-08 03:21:50 +0800

    Added Email functionality

--- App/src/components/tracker.jsx
+++ App/src/components/tracker.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import { Stepper, Step, StepLabel, Typography, Avatar } from "@mui/material";
 import Button from "@mui/material/Button";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import TextField from '@mui/material/TextField';
 
 // Define the steps in the job application process
 const steps = ["Applied", "OA", "Interview", "Offer", "Rejection"];
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: "50%",
+  maxHeight: "50%",
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  overflow: 'auto',
+};
 const JobApplicationTracker = ({ job, index, setJobs }) => {
   // Find the index of the stage to set the active step in the Stepper
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const deleteJob = async (index) => {
     try {
@@ -32,6 +52,10 @@ const JobApplicationTracker = ({ job, index, setJobs }) => {
     } catch (error) {
       console.error("There was an error!", error);
     }
+  };
+
+  const showEmail = async (index) => {
+    
   };
   const activeStep = steps.findIndex((step) => step === steps[job.stage]);
 
@@ -64,6 +88,8 @@ const JobApplicationTracker = ({ job, index, setJobs }) => {
         <Typography variant="body2" style={{ fontWeight: "800" }}>
           {job.title}
         </Typography>
+        {/* <TextField label={job.title} variant="standard"/>
+        <TextField label={job.company} variant="standard" size="small"/> */}
         <Typography
           variant="subtitle1"
           style={{ fontWeight: "300", fontSize: "14px" }}
@@ -106,6 +132,53 @@ const JobApplicationTracker = ({ job, index, setJobs }) => {
           <DeleteOutlineIcon />
         </Button>
       </div>
+      <div
+        style={{
+          width: "5%",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "20px",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOpen}
+        >
+          <MailOutlineIcon />
+        </Button>
+          <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Email Tagged to Job Application
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {job.text}
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
+      {/* <div
+        style={{
+          width: "5%",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "20px",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => deleteJob(index)}
+        >
+          <SaveOutlinedIcon />
+        </Button>
+      </div> */}
     </div>
   );
 };
